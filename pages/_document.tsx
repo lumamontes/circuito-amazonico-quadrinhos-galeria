3ca58beb5113cd103f194bcea94a348bd6f1a230
lumamontes <lumagoesmontes@gmail.com>
2025-04-19 12:56:34 -0300

    add adjustments to gallery template

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Registros do Circuito Amazônico de Quadrinhos em Macapá."
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="circuito-amazonico-quadrinhos-amapa-galeria.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Registros do Circuito Amazônico de Quadrinhos em Macapá."
           />
           <meta property="og:title" content="Next.js Conf 2022 Pictures" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Registros do Circuito Amazônico de Quadrinhos em Macapá."
           />
         </Head>
         <body className="bg-black antialiased">
