43f0563028bb715e63a5aafefb21d4437ef22041
lumamontes <lumagoesmontes@gmail.com>
2025-04-19 13:32:11 -0300

    replace generate blur with sharp

--- utils/generateBlurPlaceholder.ts
+++ utils/generateBlurPlaceholder.ts
@@ -1,5 +1,4 @@
-import imagemin from "imagemin";
-import imageminJpegtran from "imagemin-mozjpeg";
+import sharp from "sharp";
 import type { ImageProps } from "./types";
 
 const cache = new Map<ImageProps, string>();
@@ -11,15 +10,18 @@ export default async function getBase64ImageUrl(
   if (url) {
     return url;
   }
+
   const response = await fetch(
     `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`,
   );
   const buffer = await response.arrayBuffer();
-  const minified = await imagemin.buffer(Buffer.from(buffer), {
-    plugins: [imageminJpegtran()],
-  });
 
-  url = `data:image/jpeg;base64,${Buffer.from(minified).toString("base64")}`;
+  const minified = await sharp(Buffer.from(buffer))
+    .jpeg({ quality: 70 })
+    .resize(8) // make sure width is 8px
+    .toBuffer();
+
+  url = `data:image/jpeg;base64,${minified.toString("base64")}`;
   cache.set(image, url);
   return url;
 }
