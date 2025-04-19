593cf3b166bce79e48b6866b2f35bc702d6ed703
lumamontes <lumagoesmontes@gmail.com>
2025-04-19 16:05:47 -0300

    add correct og title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,9 +15,9 @@ class MyDocument extends Document {
             property="og:description"
             content="Registros do Circuito Amazônico de Quadrinhos em Macapá."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Circuito Amazônico de Quadrinhos - AP" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Circuito Amazônico de Quadrinhos - AP" />
           <meta
             name="twitter:description"
             content="Registros do Circuito Amazônico de Quadrinhos em Macapá."
